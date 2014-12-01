39ab02ba9106bf92465202994719cc0541fea4bd
Daniel Russell <danielcrussell@gmail.com>
2014-11-30 20:12:49 -0600

    Initial commit

---
+++ config/env/all.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+	app: {
+		title: 'threeaday',
+		description: 'Meal planning app',
+		keywords: 'meal, health, food'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/bootstrap/dist/css/bootstrap.css',
+				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/angular-material/angular-material.css'
+			],
+			js: [
+				'public/lib/angular/angular.js',
+				'public/lib/angular-resource/angular-resource.js', 
+				'public/lib/angular-cookies/angular-cookies.js', 
+				'public/lib/angular-animate/angular-animate.js',
+				'public/lib/angular-aria/angular-aria.js',
+				'public/lib/hammerjs/hammer.js',
+				'public/lib/angular-material/angular-material.js',
+				'public/lib/angular-touch/angular-touch.js', 
+				'public/lib/angular-sanitize/angular-sanitize.js', 
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+			]
+		},
+		css: [
+			'public/modules/**/css/*.css'
+		],
+		js: [
+			'public/config.js',
+			'public/application.js',
+			'public/modules/*/*.js',
+			'public/modules/*/*[!tests]*/*.js'
+		],
+		tests: [
+			'public/lib/angular-mocks/angular-mocks.js',
+			'public/modules/*/tests/*.js'
+		]
+	}
+};
